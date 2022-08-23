ec97b758a6e7c78e332251a4d325a472b63b2497
Ghanish Sharma <ghanishftw@gmail.com>
2022-08-23 18:25:37 +0530

    Revert "vayu: Remove LineageOS's live display"
    This reverts commit 836a0eba63ec43bf6bc4600779942c8d534629a9.
    
    Change-Id: Icd1d3fc47accbf7447aac1718cec5f40925c1863

--- device.mk
+++ device.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.vayu
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.vayu
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
